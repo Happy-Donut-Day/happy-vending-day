e57ea0705ddaaafb894283bcbf77e76ab7f1388c
Jonathan Pan <git config --global user.emailgit config --global user.emailgit config --global user.email>
2019-01-04 11:02:47 -0800

    added stuff

--- js/init.js
+++ js/init.js
@@ -3,5 +3,10 @@
 
     $('.sidenav').sidenav();
     $('.slider').slider({indicators:true});
+    $('.parallax').parallax();
+    sideBarInit();
+    sliderInit();
+    collapsibleInit();
+    $('#comment').characterCounter();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
